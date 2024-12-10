1ce7e15e0eba3964c144b732a400468fe31aef79
ZhannaIvashova <zhannaklepikova90@gmail.com>
2024-12-10 18:50:41 +0300

    feat: add task formatTime

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -250,8 +250,11 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const resultMinutes = minutes.toString().padStart(2, 0);
+  const resultSeconds = seconds.toString().padStart(2, 0);
+
+  return `${resultMinutes}:${resultSeconds}`;
 }
 
 /**
